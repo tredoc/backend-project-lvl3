6f405e1b7a5dc018b1c3e1b398720678fb57d60a
tredok <wrk.dodge@gmail.com>
2021-01-27 18:26:38 +0200

    tests: remove finally

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,23 +1,19 @@
 #!/usr/bin/env node
 
-const program = require("commander");
-const pageLoader = require("../src");
-const { red, green } = require("chalk");
+const program = require('commander')
+const pageLoader = require('../src')
+const { red, green } = require('chalk')
 
 program
-  .version("0.0.1")
-  .description("Downloads html page")
-  .option("-o, --output <location>", "enter location save to", process.cwd())
-  .arguments("<url>")
-  .helpOption("-h, --help", "output usage information")
+  .version('0.0.1')
+  .description('Downloads html page')
+  .option('-o, --output <location>', 'enter location save to', process.cwd())
+  .arguments('<url>')
+  .helpOption('-h, --help', 'output usage information')
   .action((url) => {
-    pageLoader(url, program.output)
-      .catch((err) => {
-        console.log(red(err));
-      })
-      .finally(() => {
-        console.log(green.bold("Done!"));
-      });
-  });
+    pageLoader(url, program.output).catch((err) => {
+      console.log(red(err.message))
+    })
+  })
 
-program.parse(process.argv);
+program.parse(process.argv)
